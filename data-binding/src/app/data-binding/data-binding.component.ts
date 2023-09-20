70b036a0063da9f7595b1c211590ea866ac27f85
suganame <gsuganame@gmail.com>
2023-09-20 15:36:26 -0300

    feat: interpolation and data binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css'],
+})
+export class DataBindingComponent {
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com.br/400/200/nature';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+}
